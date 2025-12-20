04a64163fcf99462747a1cc28c23da30c8f3df73
ahmadsalehinejhad-code <ahmadsalehinejhad@gmail.com>
2025-12-20 12:18:11 +0330

    Add files via upload

---
+++ selects.js
@@ -0,0 +1,14 @@
+<!--back and forward cod by noshirvani.ir-->
+<SCRIPT LANGUAGE="JavaScript">
+function goHist(a)
+{
+    history.go(a);
+}</script>
+<FORM METHOD="post">
+<center>
+<INPUT TYPE="button" VALUE="  BACK  " onClick="goHist(-1)"><span><span>      <span>
+
+<INPUT TYPE="button" VALUE="FORWARD" onClick="goHist(1)">
+</center>
+</form>
+<!--back and forward cod by noshirvani.ir-->
